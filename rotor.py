ffccd203a69c3903203b034cf8871515f8542ea0
nvoid <nvoidtst@pm.me>
2021-04-15 22:03:34 +0100

    started rotor implementation

---
+++ rotor.py
@@ -0,0 +1,12 @@
+rotor_i = "EKMFLGDQVZNTOWYHXUSPAIBRCJ"
+
+def create_rotor(rotor,number):
+    rotor_finished = {}
+    for n,i in emumerate(rotor):
+        rotor_finished[n].append(ord(i)-97)
+
+
+
+if __name__ == "__main__":
+        # execute only if run as a script
+        main()
